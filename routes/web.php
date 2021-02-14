d19c331e7c06d9d5b98dc2cf04af1a9b589a69ef
Rabiul Awal <globalidealbd@gmail.com>
2021-02-14 11:31:48 +0600

    Date: 14-02-2021

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,68 @@
 |
 */
 
+
+ /*
+ |---------------------------------------------------------------------------
+ |Test Route 
+ |---------------------------------------------------------------------------
+ */
+
+// Available Routes
+// Route::get('uri','callback'); 
+// Route::post('uri','callback'); 
+// Route::delete('uri','callback'); 
+// Route::put('uri','callback'); 
+// Route::patch('uri','callback'); 
+// Route::options('uri','callback'); 
+
+
+Route::any('/', function(){
+	// Write your route or code here 
+});
+
+Route::match(['get','post'],'/',function(){
+	// Write your route or code here  
+}); 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testroute',function() {
+	echo "This is route test";
+});
+
+Route::get('/here',function() {
+	echo "This is here test";
+});
+
+Route::get('/there',function() {
+	echo "This is there test";
+});
+
+Route::get('user/profile', function(){
+
+})->name('profile');
+
+
+
+
+
+// Route::redirect('/here','/there');
+// Route::get('/test','UserController@index');
+
+// Route::view('/welcome', 'welcome', ['namehttps://images.prothomalo.com/prothomalo-bangla%2F2021-02%2F80cd902c-8054-4639-b900-60251ff5c329%2FBNP.JPG?auto=format%2Ccompress&format=webp&w=720&dpr=1.0' => 'Primetech']);
+
+
+ /*
+ |---------------------------------------------------------------------------
+ |Project Route 
+ |---------------------------------------------------------------------------
+ */
+
+
+
+
+
+// https://laravel.com/docs/6.x/routing Date : 11-02-2021   Date: 07:26 PM
